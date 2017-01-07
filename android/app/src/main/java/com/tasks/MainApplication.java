577717d33162fd3cbd99616c56e2a31a38c478e9
Cadel Watson <cadel@cadelwatson.com>
2017-01-07 15:08:48 +1100

    Add dependencies for react-native-elements

--- android/app/src/main/java/com/tasks/MainApplication.java
+++ android/app/src/main/java/com/tasks/MainApplication.java
@@ -4,8 +4,8 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
-import cl.json.RNSharePackage;
 import com.oblador.vectoricons.VectorIconsPackage;
+import cl.json.RNSharePackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -27,8 +27,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNSharePackage(),
-            new VectorIconsPackage()
+            new VectorIconsPackage(),
+            new RNSharePackage()
       );
     }
   };
