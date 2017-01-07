54984ebbff5b9d705ba46f0db6eceea29f1ac0c6
Cadel Watson <cadel@cadelwatson.com>
2017-01-08 10:26:24 +1100

    Move task list to state

--- src/scenes/MainScene.js
+++ src/scenes/MainScene.js
@@ -4,47 +4,47 @@ import {View, StyleSheet} from 'react-native'
 import {Button, Icon, ButtonGroup, Text, List, ListItem} from 'react-native-elements'
 import ScrollableTabView, {DefaultTabBar, } from 'react-native-scrollable-tab-view'
 
-// A list of dummy todos
-const todos = [
-  {
-    title: 'Do this upcoming thing',
-    id: 123,
-    daysRemaining: 3,
-    status: 0,
-  },
-  {
-    title: 'Do this other upcoming thing',
-    id: 124,
-    daysRemaining: 5,
-    status: 0,
-  },
-  {
-    title: 'Do this active thing',
-    id: 125,
-    daysRemaining: 3,
-    status: 1,
-  },
-  {
-    title: 'Do this other active thing',
-    id: 126,
-    daysRemaining: 5,
-    status: 1,
-  },
-  {
-    title: 'Do this completed thing',
-    id: 127,
-    daysRemaining: 3,
-    status: 2,
-  },
-  {
-    title: 'Do this other completed thing',
-    id: 128,
-    daysRemaining: 22,
-    status: 2,
-  },
-];
 
 export default class MainScene extends Component {
+
+  state = {tasks: [
+    {
+      title: 'Do this upcoming thing',
+      id: 123,
+      daysRemaining: 3,
+      status: 0,
+    },
+    {
+      title: 'Do this other upcoming thing',
+      id: 124,
+      daysRemaining: 5,
+      status: 0,
+    },
+    {
+      title: 'Do this active thing',
+      id: 125,
+      daysRemaining: 3,
+      status: 1,
+    },
+    {
+      title: 'Do this other active thing',
+      id: 126,
+      daysRemaining: 5,
+      status: 1,
+    },
+    {
+      title: 'Do this completed thing',
+      id: 127,
+      daysRemaining: 3,
+      status: 2,
+    },
+    {
+      title: 'Do this other completed thing',
+      id: 128,
+      daysRemaining: 22,
+      status: 2,
+    },
+  ]};
 
   // Open the edit screen for the given list item
   editListItem = (id) => {
@@ -52,12 +52,13 @@ export default class MainScene extends Component {
 
   // Promote the given list item from Upcoming/Active -> Completed
   completeListItem = (id) => {
+    let {tasks} = this.state;
     // Find the task with the right ID
-    const index = todos.findIndex(x => x.id === id);
+    const index = tasks.findIndex(x => x.id === id);
     // Set its state to completed
-    todos[index].status = 2;
-    // Force a re-render
-    this.setState(this.state);
+    tasks[index].status = 2;
+    // Set the new state
+    this.setState({tasks});
   };
 
   // Given a task object and index, return a ListItem
@@ -75,6 +76,7 @@ export default class MainScene extends Component {
   }
 
   render() {
+    const {tasks} = this.state;
     return (
       <View style={styles.container}>
 
@@ -83,19 +85,19 @@ export default class MainScene extends Component {
         >
           <List tabLabel={'Completed'}>
             {
-              todos.filter(x => x.status === 2)
+              tasks.filter(x => x.status === 2)
                 .map((listItem, index) => this.renderListItem(listItem, index))
             }
           </List>
           <List tabLabel={'Active'}>
             {
-              todos.filter(x => x.status === 1)
+              tasks.filter(x => x.status === 1)
                    .map((listItem, index) => this.renderListItem(listItem, index))
             }
           </List>
           <List tabLabel={'Upcoming'}>
             {
-              todos.filter(x => x.status === 0)
+              tasks.filter(x => x.status === 0)
                 .map((listItem, index) => this.renderListItem(listItem, index))
             }
           </List>
