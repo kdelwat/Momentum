9d7b2015e8d1274dac42e9c790cd52e601ce3248
Cadel Watson <cadel@cadelwatson.com>
2017-01-07 13:33:39 +1100

    Add Navigator to the main scene

--- index.android.js
+++ index.android.js
@@ -1,18 +1,29 @@
+/*The index file simply contains the navigator, which delegates to individual scenes.*/
+
 import React, { Component } from 'react';
 import {
   AppRegistry,
-  Text,
-  View
+  Navigator
 } from 'react-native';
 
+import MainScene from './src/scenes/MainScene'
+
 export default class Tasks extends Component {
+
+  /*
+   renderScene is called by the navigator when a route is pushed. The scene doing the pushing
+   must provide a route ID to link to another scene.
+   */
+  renderScene(route, navigator) {
+    switch(route.id) {
+      case 'Main':
+        return <MainScene navigator={navigator} />
+    }
+  };
+
   render() {
     return (
-      <View>
-        <Text>
-          Welcome to React Native!
-        </Text>
-      </View>
+      <Navigator initialRoute={{id: 'Main'}} renderScene={this.renderScene} />
     );
   }
 }
---
+++ src/scenes/MainScene.js
@@ -0,0 +1,14 @@
+/*The main scene of the app: a list of tasks*/
+import React, {Component} from 'react'
+import {View, Text} from 'react-native'
+
+export default class MainScene extends Component {
+
+  render() {
+    return (
+      <View>
+        <Text>Main Scene</Text>
+      </View>
+    )
+  }
+}
