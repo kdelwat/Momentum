2043eae18acccb8d116fe407921acc84f7f2e6fa
Cadel Watson <cadel@cadelwatson.com>
2017-01-08 18:31:51 +1100

    Add editing of active and deadline datetime

--- src/scenes/EditScene.js
+++ src/scenes/EditScene.js
@@ -3,11 +3,20 @@
 import React, {Component} from 'react'
 import {View, StyleSheet} from 'react-native'
 import {FormLabel, FormInput, Text, Button} from 'react-native-elements'
+import DatePicker from 'react-native-datepicker'
+import moment from 'moment'
+import 'moment/locale/en-au'
+
+const TIMEDATE_FORMAT = 'YYYY-MM-DD HH:mm';
 
 export default class EditScene extends Component {
 
+  // Initialise state with the parameters of the edited task.
+  // This means that any unchanged field will remain the same.
   state = {
-    title: '',
+    title: this.props.task.title,
+    active: this.props.task.active.format(TIMEDATE_FORMAT),
+    deadline: this.props.task.deadline.format(TIMEDATE_FORMAT),
   };
 
   // When finished editing, create a new task and call the callback function with the task id
@@ -18,8 +27,8 @@ export default class EditScene extends Component {
       title: this.state.title,
       id: oldTask.id,
       completed: oldTask.completed,
-      active: oldTask.active,
-      deadline: oldTask.deadline,
+      active: moment(this.state.active),
+      deadline: moment(this.state.deadline),
     };
     this.props.callback(this.props.task.id, newTask);
     this._return();
@@ -30,7 +39,10 @@ export default class EditScene extends Component {
     this.props.navigator.pop();
   }
 
-  onChangeText = (title) => this.setState({title});
+  // Listeners that update the state on input
+  onChangeText = (title) => this.setState({title: title});
+  onChangeDeadline = (date) => this.setState({deadline: date});
+  onChangeActive = (date) => this.setState({active: date});
 
   render() {
     return (
@@ -41,6 +53,20 @@ export default class EditScene extends Component {
         <FormLabel>Title</FormLabel>
         <FormInput placeholder={this.props.task.title}
                    onChangeText={this.onChangeText}/>
+        <FormLabel>Deadline</FormLabel>
+        <DatePicker
+          date={this.state.deadline}
+          mode="datetime"
+          format={TIMEDATE_FORMAT}
+          onDateChange={this.onChangeDeadline}
+        />
+        <FormLabel>Active</FormLabel>
+        <DatePicker
+          date={this.state.active}
+          mode="datetime"
+          format={TIMEDATE_FORMAT}
+          onDateChange={this.onChangeActive}
+        />
         <Button title="Done"
                 raised
                 onPress={this.finished}/>
