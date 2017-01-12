1b74ab05a876f8049a5448bb1efd3405974e1f9b
Cadel Watson <cadel@cadelwatson.com>
2017-01-12 18:00:56 +1100

    Add description to splash screen and format

--- src/scenes/SplashScene.js
+++ src/scenes/SplashScene.js
@@ -6,7 +6,7 @@ import {Text, Icon} from 'react-native-elements'
 import {styles, colors} from '../Styles'
 import TitleBar from '../components/TitleBar'
 
-INTRODUCTION_TEXT = 'This is the introduction';
+const TEXT_PADDING=10;
 
 export default class SplashScene extends Component {
 
@@ -18,16 +18,33 @@ export default class SplashScene extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <TitleBar title={'Welcome to Momentum'} />
+        <TitleBar title={'Welcome to Momentum'}/>
 
-        <View style={[styles.formContainer, {flex: 1}]}>
-          <Text>{INTRODUCTION_TEXT}</Text>
+        <View style={[styles.formContainer, {flex: 1, marginTop: 30}]}>
+          <Text style={{marginBottom: TEXT_PADDING}}>Momentum is a task management app with a simple philosophy.</Text>
+          <Text style={{marginBottom: TEXT_PADDING}}>
+            <Text style={{fontWeight: 'bold'}}>Tasks</Text> are items with a set finish point.
+            They have three states:
+          </Text>
+          <Text style={{marginBottom: TEXT_PADDING}}>
+            {'\t\t'}<Text style={{fontWeight: 'bold'}}>Active</Text> tasks are those which are ready to work on.{'\n'}
+            {'\t\t'}<Text style={{fontWeight: 'bold'}}>Upcoming</Text> tasks are those yet to become active.{'\n'}
+            {'\t\t'}<Text style={{fontWeight: 'bold'}}>Completed</Text> tasks are those already finished.{'\n'}
+          </Text>
+          <Text style={{marginBottom: TEXT_PADDING}}>
+            When a task is created, both a deadline and a date to become active can be specified.
+            To edit a task, press on it. To complete a task, press and hold.
+          </Text>
+          <Text>
+            <Text style={{fontWeight: 'bold'}}>Notes</Text> are items with no deadline.
+            They are automatically classed as active until completed.
+          </Text>
         </View>
         <View style={styles.horizontal}>
           <Icon
             name={'done'}
             reverse
-            color={colors.main}
+            color={colors.priority4}
             onPress={this.finished}/>
         </View>
       </View>
